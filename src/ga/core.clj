768f96efdeb1710cc54fe6579c92083f3c93810d
Prasad Kulkarni <prasad.fromgroup@gmail.com>
2016-10-16 19:05:52 +0100

    Adding binary fitness calculation logic

--- src/ga/core.clj
+++ src/ga/core.clj
@@ -5,3 +5,10 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn fitness-calc
+  [individual solution]
+  "This method returns number of matching bits in the individual and the solution. This method makes an assumption that individual and solution will be of the same length"
+  ((frequencies (map (fn [a b] (cond
+                             (= a b) 1
+                             :else 0)) individual solution)) 1))
