7f9d8cd026fb611b7e410c7299def700578ce921
Prasad Kulkarni <prasad.fromgroup@gmail.com>
2016-10-16 19:05:14 +0100

    Initial commit.

---
+++ src/ga/core.clj
@@ -0,0 +1,7 @@
+(ns ga.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/ga/core_test.clj
@@ -0,0 +1,7 @@
+(ns ga.core-test
+  (:require [clojure.test :refer :all]
+            [ga.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
